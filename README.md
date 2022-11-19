9ad302c1aad60b6e564f059d2d7445b29fb219f3
Jason Eagleston <jason@yuken.xyz>
2022-11-19 17:25:18 -0500

    Added README, did some work on chat messages.

---
+++ README.md
@@ -0,0 +1,1 @@
+2D MUD written in Godot, to replace the BYOND version of Roleplay Rebirth/Finale.
--- src/MainMenu.gd
+++ src/MainMenu.gd
@@ -24,10 +24,13 @@ func _ready():
 	client = NetworkedMultiplayerENet.new()
 	get_tree().connect("connected_to_server", self, "_connected_to_server")
 	get_tree().connect("server_disconnected", self, "_disconnect_from_server")
+	client.connect("connection_failed", self, "_connection_failed")
 	var connect_button = $PanelContainer/Panel/Connect
 	connect_button.connect("pressed", self, "_on_connect_pressed")
 	var quit_button = $PanelContainer/Panel/Quit
 	quit_button.connect("pressed", self, "_on_quit_pressed")
+func _connection_failed():
+	set_process(false)
 func _connected_to_server():
 	set_process(true)
 func _disconnect_from_server():
@@ -43,19 +46,36 @@ func _peer_connected(id):
 func handle_packet_server(id, dt):
 	var pkt: PoolByteArray = server.get_packet()
 	var pkt_type = pkt[0]
+	var _client: Client = clients[id]
 	server.set_target_peer(id)
-	pkt.remove(0)
 	match pkt_type:
 		CHAT_MESSAGE:
+			# Chat message structure is TYPE, CHARACTER_NAME_SIZE, CHARACTER_NAME, DATA
 			print(id, pkt.get_string_from_utf8())
-			server.set_target_peer(-id)
-			pkt.insert(0, CHAT_MESSAGE)
-			server.put_packet(pkt)
+			server.set_target_peer(server.TARGET_PEER_BROADCAST)
+			var new_packet: PoolByteArray = PoolByteArray()
+			var character: PoolByteArray = _client.character.to_utf8()
+			new_packet.append(CHAT_MESSAGE)
+			var name_size = 0
+			new_packet.append(character.size())
+			new_packet.append_array(character)
+			new_packet.append_array(pkt.subarray(1, pkt.size()))
+			server.put_packet(new_packet)
 		_: pass
 func handle_packet_client():
 	var pkt: PoolByteArray = client.get_packet()
 	var pkt_type = pkt[0]
-	pkt.remove(0)
+	match pkt_type:
+		CHAT_MESSAGE:
+			var name_size = pkt[1]
+			var name_buf: PoolByteArray = PoolByteArray()
+			var name: String = ""
+			for i in range(name_size):
+				name_buf.append(pkt[i + 1])
+			name = name_buf.get_string_from_utf8()
+			var message = pkt.subarray(name_size, pkt.size() - name_size - 2)
+			print(message)
+		_: pass
 func _process(dt):
 	if is_network_master():
 		server.poll()
